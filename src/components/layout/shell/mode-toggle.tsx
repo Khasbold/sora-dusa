30991df002f4d1f16a64c61a5c67871d6477e8e3
khasbold.t <khasbold.t@unitel.mn>
2025-04-14 14:26:39 +0800

    init

---
+++ src/components/layout/shell/mode-toggle.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import * as React from "react"
+import { MoonIcon, SunIcon } from "lucide-react"
+import { Button } from "@medusajs/ui"
+import { useTheme } from "../../../providers/theme-provider"
+
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme()
+  const toggleTheme = React.useCallback(() => {
+    setTheme(theme === "dark" ? "light" : "dark")
+  }, [theme, setTheme])
+
+  return (
+    <Button
+      variant="primary"
+      className="group/toggle h-8 w-8 px-0"
+      onClick={toggleTheme}
+    >
+      <SunIcon className="hidden [html.dark_&]:block" />
+      <MoonIcon className="hidden [html.light_&]:block" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  )
+}
